edc09d5f8983c9371de0422ac81fb403781449d1
ryan-pratt <rpratt.dev@gmail.com>
2020-07-18 22:44:29 -0600

    added socket.io

--- src/game.tsx
+++ src/game.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import io from 'socket.io-client';
+import api from './utilities/api';
 import Deck from './objects/deck';
 import DeckCard from './objects/deckCard';
 import './styles/game.scss';
@@ -19,13 +21,24 @@ class Game extends React.Component<GameProps, GameState> {
       deck: new Deck()
     };
   }
+
+  componentDidMount() : void {
+    const { gameCode } = this.props;
+    const socket = io.connect("http://localhost:5000", {
+      reconnection: true
+    });
+    socket.emit('join', {room: gameCode});
+    socket.on('test', (message : any) => {
+      console.log('test', message);
+    });
+  }
   
   render() : JSX.Element {
     const card : DeckCard = this.state.deck.draw();
 
     return (
       <div className="game">
-        <img src={card.image} alt={card.name} />
+        <img src={card.image} alt={card.name} onClick={async () => await api.endTurn() } />
         <p>this is the game content</p>
         {this.props.isHosting ? <p>this user is hosting</p> : <p>this user is not hosting</p>}
         <p>the game code is: {this.props.gameCode}</p>
--- src/utilities/api.ts
+++ src/utilities/api.ts
@@ -2,11 +2,17 @@ import axios from 'axios';
 
 const server : string = "http://localhost:5000";
 
+axios.defaults.withCredentials = true
+
 export default {
   startSession: async (code : string) : Promise<void> => {
     await axios.post(`${server}/start`, {code: code});
   },
   joinSession: async (code : string) : Promise<void> => {
     await axios.put(`${server}/start`, {code: code});
+  },
+
+  endTurn: async () : Promise<void> => {
+    await axios.post(`${server}/turn`)
   }
 }
