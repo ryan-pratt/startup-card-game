52baee39d1373ab3b72c6a68eb762ac6b32176b8
ryan-pratt <rpratt.dev@gmail.com>
2020-07-25 22:09:01 -0600

    renamed session endpoints

--- src/utilities/api.ts
+++ src/utilities/api.ts
@@ -6,10 +6,10 @@ axios.defaults.withCredentials = true
 
 export default {
   startSession: async (code : string) : Promise<void> => {
-    await axios.post(`${server}/start`, {code: code});
+    await axios.post(`${server}/session`, {code: code});
   },
   joinSession: async (code : string) : Promise<string> => {
-    const response : AxiosResponse = await axios.put(`${server}/start`, {code: code});
+    const response : AxiosResponse = await axios.put(`${server}/session`, {code: code});
     return response.data
   },
 
